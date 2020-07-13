eeeab5f5a625b005bcba23ba7fdea60039960bb6
bernard yohann <yohannb215@gmail.com>
2020-07-13 15:29:26 +0200

    pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
 
   before_action :set_locale
   before_action :authenticate_user!
+  include Pundit
+
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   def default_url_options
     { locale: I18n.locale }
@@ -16,6 +20,10 @@ class ApplicationController < ActionController::Base
     cookies[:locale] = { value: locale,
                          expires: 30.days.from_now
                         }
+  end
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
 end
---
+++ app/policies/clothe_policy.rb
@@ -0,0 +1,43 @@
+class ClothePolicy < ApplicationPolicy
+
+  attr_reader :user, :clothe
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def initialize(user, clothe)
+    @user = user
+    @clothe = clothe
+  end
+
+  def index?
+    return true
+  end
+
+  def show?
+    return true
+  end
+
+  def create?
+    return true
+  end
+
+  def new?
+    return true
+  end
+
+  def edit?
+    return true
+  end
+
+  def update?
+    return true
+  end
+
+  def destroy?
+    return true
+  end
+end
