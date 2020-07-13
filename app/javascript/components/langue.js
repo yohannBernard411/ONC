cb917cde422c81dd37c38fb6351ca99e0164dd36
bernard yohann <yohannb215@gmail.com>
2020-07-13 12:21:36 +0200

    change_image_url

--- app/javascript/components/langue.js
+++ app/javascript/components/langue.js
@@ -3,13 +3,13 @@ const displayFlag = () => {
   const label = document.getElementById("labelLangue").innerText;
 
   if (label == "fr"){
-      flag.style.backgroundImage = "url('assets/frenchFlag.png')";
+      flag.style.backgroundImage = "url('/images/frenchFlag.png')";
   }
   if (label == "en"){
-      flag.style.backgroundImage = "url('assets/englishFlag.png')";
+      flag.style.backgroundImage = "url('/images/englishFlag.png')";
   }
   if (label == "es"){
-      flag.style.backgroundImage = "url('assets/spainFlag.png')";
+      flag.style.backgroundImage = "url('/images/spainFlag.png')";
   }
 }
 
