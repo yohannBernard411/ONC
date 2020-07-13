4f506cee170aaf0fde9a3bd75bf56e1139c9e718
bernard yohann <yohannb215@gmail.com>
2020-07-13 12:01:16 +0200

    internationalisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+  before_action :set_locale
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  private
+
+  def set_locale
+    locale = params[:locale] || cookies[:locale] || I18n.default_locale
+    @langue = locale
+    I18n.locale = locale
+    cookies[:locale] = { value: locale,
+                         expires: 30.days.from_now
+                        }
+  end
+
 end
