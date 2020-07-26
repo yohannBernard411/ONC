50dccffa8a6575b22f020aeeb4c475d3e218d7a8
bernard yohann <yohannb215@gmail.com>
2020-07-26 21:40:34 +0200

    find_by

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ApplicationRecord
   end
 
   def self.from_facebook(auth)
-    where(facebook_id: auth.uid, email: auth.email).first_or_create! do |user|
+    find_by(facebook_id: auth.uid).or find_by(email: auth.email).first_or_create! do |user|
       user.email = auth.info.email
       user.username = auth.info.name
       user.password = Devise.friendly_token[0, 20]
