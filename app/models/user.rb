c2d09f5e382ca0f82daaa2e515178d2e75140388
bernard yohann <yohannb215@gmail.com>
2020-07-27 10:20:26 +0200

    connection_production

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,6 @@ class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   def self.find_first_by_auth_conditions(warden_conditions)
-    
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
       where(conditions.to_hash).where("lower(username) = :value OR lower(email) = :value", value: login.downcase).first
@@ -27,7 +26,7 @@ class User < ApplicationRecord
   end
 
   def self.from_facebook(auth)
-    where(facebook_id: auth.uid) || where(email: auth.email).first_or_create! do |user|
+    (where(email: auth.info.email)||where(facebook_id: auth.uid)).first_or_create do |user|
       user.email = auth.info.email
       user.username = auth.info.name
       user.password = Devise.friendly_token[0, 20]
