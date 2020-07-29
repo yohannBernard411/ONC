bbbf5eda305d13b6f51dfeacf6fcfd587d9e62cd
bernard yohann <yohannb215@gmail.com>
2020-07-29 17:27:57 +0200

    validates

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :clothe
 
-  validations :title, presence: true
-  validations :content, presence: true
-  validations :note, presence: false, inclusion: { in: 0..5 }
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :note, presence: false, inclusion: { in: 0..5 }
 
 
 end
