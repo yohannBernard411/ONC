f98f406218a45f849d6d509c1826ad551a644849
bernard yohann <yohannb215@gmail.com>
2020-07-29 22:44:21 +0200

    mailgun

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'emailing@bernardyohann.fr'
+  API_KEY = ENV['MAILGUN_API_KEY']
+  API_URL = "https://api:#{API_KEY}@api.mailgun.net/v2/ocenatcreations.herokuapp.com"
   layout 'mailer'
 end
--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: 'gmail.com',
-  user_name: ENV['GMAIL_ADDRESS'],
-  password: ENV['GMAIL_APP_PASSWORD'],
-  authentication: :login,
-  enable_starttls_auto: true
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'ocenatcreations.herokuapp.com',
+  :authentication => :plain,
 }
+ActionMailer::Base.delivery_method = :smtp
