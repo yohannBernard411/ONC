c76774d3de9458622071843cab3915b3c887c1c8
bernard yohann <yohannb215@gmail.com>
2020-07-29 19:35:19 +0200

    user_mailer_adress

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,5 @@
 class UserMailer < ApplicationMailer
   def welcome
-    # @user = params[:user] # Instance variable => available in view
-    # mail(to: @user.email, subject: 'Welcome to OceNatCreations')
-    # # This will render a view in `app/views/user_mailer`!
+
   end
 end
