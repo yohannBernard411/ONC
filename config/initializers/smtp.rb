8eff64563c5b718b0d921130a84d9dab89e81eac
bernard yohann <yohannb215@gmail.com>
2020-07-31 16:38:24 +0200

    authentication_false

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -4,6 +4,6 @@ ActionMailer::Base.smtp_settings = {
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
   :domain         => 'ocenatcreations.herokuapp.com',
-  :authentication => :plain,
+  :authentication => false # :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
