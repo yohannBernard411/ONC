de771d5684527ad9c1919ec6d41e0264ba442e99
bernard yohann <yohannb215@gmail.com>
2020-08-14 17:18:43 +0200

    fghjk

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'yohannb215@gmail.com',
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'ocenatcreations.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,15 +63,8 @@ Rails.application.configure do
   # mailer avec sendgrid
   config.action_mailer.default_url_options = { host: "ocenatcreations.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name: 'newother',
-    password: ENV['SENDGRID_API_KEY'],
-    domain: ENV['SENDGRID_DOMAIN'],
-    address: ENV['SENDGRID_SERVER'],
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+
+
   # config.action_mailer.delivery_method = :sendgrid_actionmailer
   # config.action_mailer.sendgrid_actionmailer_settings = {
   #   api_key: ENV['SENDGRID_API_KEY'],
