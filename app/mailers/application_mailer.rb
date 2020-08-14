53a9108884385e00b4a9045ddebaa06b83bb7b8c
bernard yohann <yohannb215@gmail.com>
2020-08-14 15:31:09 +0200

    mail_work_local

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  # using SendGrid's Ruby Library
-  # https://github.com/sendgrid/sendgrid-ruby
+  default from: "courrier@ocenatcreations.herokuapp.com"
+  layout 'mailer'
   
 end
