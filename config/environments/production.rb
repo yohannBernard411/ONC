2b24910ea69fa587dc864d488706630882268b5e
bernard yohann <yohannb215@gmail.com>
2020-08-14 16:17:22 +0200

    addmastermailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "ONC_production"
 
   # mailer avec sendgrid
+  config.action_mailer.default_url_options = { host: "ocenatcreations.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name: 'newother',
